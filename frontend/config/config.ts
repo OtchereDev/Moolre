6ab7d5f1de0e05533b0e83a5c1d53141dc0e31f5
Oliver Otchere <Oliverotchere4@gmail.com>
2022-08-01 04:09:21 +0000

    fixed frontend

--- frontend/config/config.ts
+++ frontend/config/config.ts
@@ -1,2 +1,2 @@
-export const APIURL = "https://moolre.herokuapp.com/api";
-export const FRONTEND_API = "https://moolre.vercel.app";
+export const APIURL = "https://moolre.herokuapp.com";
+export const FRONTEND_API = "https://moolre.vercel.app/api";
